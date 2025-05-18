a2e80ad22c7016c232b305ea175276b29ae78114
fbbp <foobarbazpro@gmail.com>
2025-05-18 21:08:44 +0900

    .nojekyllファイルを作成他

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/todo-codex/',
+  publicDir: 'public',
   plugins: [
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/.nojekyll',
+          dest: ''
+        }
+      ]
+    }),
     VitePWA({
       registerType: 'prompt',
       strategies: 'injectManifest',
@@ -36,6 +46,15 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/.nojekyll',
+          dest: './',
+          flatten: true
+        }
+      ]
     })
   ],
 });
