a98ea7a10aca2bf5d040e845eb281fa1cdc6adb0
fbbp <foobarbazpro@gmail.com>
2025-05-18 21:20:01 +0900

    vite-plugin-static-copyプラグインを削除してシンプルな構成に変更:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,14 +8,6 @@ export default defineConfig({
   publicDir: 'public',
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/.nojekyll',
-          dest: ''
-        }
-      ]
-    }),
     VitePWA({
       registerType: 'prompt',
       strategies: 'injectManifest',
@@ -46,15 +37,6 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/.nojekyll',
-          dest: './',
-          flatten: true
-        }
-      ]
     })
   ],
 });
