0e3a2c685d7ca802793dbbba37f9d0f5a12f6348
fbbp <fbbp@users.noreply.github.com>
2025-05-20 01:09:42 +0900

    fix PWA prompt types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,22 @@ declare module 'virtual:pwa-register' {
   export type { RegisterSWOptions };
   export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
 }
+
+declare module 'virtual:pwa-register/react' {
+  import type { Dispatch, SetStateAction } from 'react';
+  import type { RegisterSWOptions } from 'vite-plugin-pwa/types';
+
+  export function useRegisterSW(
+    options?: RegisterSWOptions,
+  ): {
+    offlineReady: [boolean, Dispatch<SetStateAction<boolean>>];
+    needRefresh: [boolean, Dispatch<SetStateAction<boolean>>];
+    updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
+  };
+}
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+  prompt(): Promise<void>;
+}
